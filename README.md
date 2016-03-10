dfb64ea49361a2e3421e82fb5384945b5c79f5b9
Avi Chad-Friedman <ajc2212@columbia.edu>
2016-03-10 17:05:15 -0500

    readme

---
+++ README.md
@@ -0,0 +1,9 @@
+This program takes in a stream of city bike  data from www.citibikenyc.com and looks for changes in the
+number of available bikes (bike rental events).  Using the API, the website displays a histogram of the different
+stations across the city and how many rentals have occured at each one in the past 10 minutes.
+
+1. Start the local redis server: ```redis-server /usr/local/etc/redis.conf```
+2. Run the pipeline or manually start the programs```cd bin; ./pipeline.sh```
+3. Start the API server ```python bin/city_bike_api.py```
+4. Start the alerting system ```python bin/city_bike_alert_bot.py```
+5. Start the website```python -m SimpleHTTPServer 8080```
--- bin/city_bike_alert_bot.py
+++ bin/city_bike_alert_bot.py
@@ -21,8 +21,8 @@ current_entropy = response['entropy']
 while True:
 	delta = current_entropy - previous_entropy
 	print delta
-	# if(abs(delta) > 0.01):
-	if(True):
+	#if the entropy has changed a significant amount, then create an alert using the API
+	if(abs(delta) > 0.01):
 		data = urllib.urlencode({'message' : 'Entropy changed'})
 		u = urllib2.urlopen('http://127.0.0.1:5000/post_alert', data)
 
@@ -30,24 +30,4 @@ while True:
 	previous_entropy = current_entropy
 	current_entropy = response['entropy']
 	time.sleep(5)
-	
-
-
-	# line = sys.stdin.readline()
-	# json_data = json.loads(line)
-	# avg_tweet_rate = json_data["avg_tweet_rat"]
-	# print avg_tweet_rate
-	# #observing the stream has shown that people tweet about kanye's album about once every 10 seconds
-	# #if it dips below 8, assume he is extram popular 
-	# if(avg_tweet_rate < 8 and alert["popularity"] < 1):
-	# 	print "TLOP is trending"
-	# 	api.statuses.update(status="TLOP is trending")
-
-	# 	alert["popularity"] = 1
-	# #if it goes about 12, assume people don't care too much about yeezy at the moment :/
-	# if(avg_tweet_rate > 12 and alert["popularity"] > -1):
-	# 	print "No one's talking about TLOP :/"
-	# 	api.statuses.update(status="No one's talking about TLOP :/")
-	# 	alert["popularity"] = -1
-	# sys.stdout.flush()
 	
